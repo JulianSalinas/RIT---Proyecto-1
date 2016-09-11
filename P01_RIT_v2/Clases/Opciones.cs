edb2368cb7f7598ce230328d8d555a1de2e5657e
Julian-PC <Julian-PC@JULIAN-PC>
2016-09-11 13:32:22 -0600

    Agregado archivo y clase de opciones

--- P01_RIT_v2/Clases/Opciones.cs
+++ P01_RIT_v2/Clases/Opciones.cs
@@ -1,23 +1,26 @@
 ﻿using System;
+using System.IO;
 using System.Xml;
 
 namespace P01_RIT_v2.Clases
 {
+
+    /*Clase para obtener y cambiar la direcciones de los diferentes archivos*/
+    /*Sirve tambien para cargar las ultimas rutas usadas por medio de un XML*/
+
 
     class Opciones
     {
 
         private static Opciones instance;
         public string RutaOpciones;
-        public string RutaColeccion;
         public string RutaStopWords;
+        public string RutaColeccion;     
         public string RutaArchivos;
 
+
         protected Opciones() {
-            RutaOpciones = Environment.SpecialFolder.MyDocuments + "\\RIT\\Opciones.xml";
-            RutaColeccion = Environment.SpecialFolder.MyDocuments + "\\RIT\\Coleccion";
-            RutaStopWords = Environment.SpecialFolder.MyDocuments + "\\RIT\\Stopwords.xml";
-            RutaArchivos = Environment.SpecialFolder.MyDocuments + "\\RIT\\Archivos";
+            cargarOpciones();
         }
 
         public static Opciones Instance{
@@ -28,23 +31,42 @@ namespace P01_RIT_v2.Clases
             }
         }
 
-        public void guardarOpciones() {
-            try {
-                XmlDocument opciones = new XmlDocument();
-            }
-            catch(Exception) {
-
-            }
-        }
+        /*Lee las rutas del XML opciones*/
 
         public void cargarOpciones() {
-            try {
+            RutaOpciones = "...\\...\\Recursos\\Opciones.xml";
+            RutaStopWords = "...\\...\\Recursos\\Stopwords.xml";
 
-            }
-            catch ( Exception ) {
+            XmlDocument docOpciones = new XmlDocument();
+            docOpciones.Load(RutaOpciones);
 
-            }
+            XmlElement opciones = docOpciones.DocumentElement;
+            XmlElement rutaC = (XmlElement) opciones.GetElementsByTagName("RutaColeccion")[0];
+            XmlElement rutaA = (XmlElement) opciones.GetElementsByTagName("RutaArchivos")[0];
+            RutaColeccion = rutaC.FirstChild.Value;
+            RutaArchivos = rutaA.FirstChild.Value;
         }
+
+        /*Sobrescribe el archivo de opciones del proyecto*/
+
+        public void guardarOpciones() {
+            XmlWriter writer = XmlWriter.Create(RutaOpciones);
+            writer.WriteStartElement("Opciones");
+            writer.WriteElementString("RutaColeccion", RutaColeccion);
+            writer.WriteElementString("RutaArchivos", RutaArchivos);
+            writer.WriteEndElement();
+            writer.Flush();
+        }
+        
+
+        override public string ToString() {
+            string str = "Ruta de opciones: " + RutaOpciones + "\n";
+            str += "Ruta de archivos: " + RutaArchivos + "\n";
+            str += "Ruta de coleccion: " + RutaColeccion + "\n";
+            str += "Ruta de stopwords: " + RutaStopWords + "\n";
+            return str;
+        }
+      
 
 
     }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RIT---Proyecto-1
+
+# Archivo de opciones contiene las ultimas rutas donde guardar y crear los archivos
