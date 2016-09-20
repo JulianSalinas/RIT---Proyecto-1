1522f33e42be974b4aa4b7b4d4137f8bb177a33f
Esteban Fonseca Montoya <kohaimomomiya@gmail.com>
2016-09-20 11:33:34 -0600

    Agrega búsqueda interna de datos en el documento.

--- P01_RIT_v2/Clases/Documento.cs
+++ P01_RIT_v2/Clases/Documento.cs
@@ -34,35 +34,179 @@ namespace P01_RIT_v2.Clases
             XmlDocument = null;
         }
 
+        /// <summary>
+        /// Obtiene la cantidad de apariciones de un término dentro del documento.
+        /// </summary>
+        /// <param name="term">Término consultado.</param>
+        /// <returns>Cantidad de apariciones del término. Si el término no aparece, se retorna cero.</returns>
         public int countTermino( string term ) {
             return Terminos.FindAll(x => x.ToString() == term).Count;
         }
 
+        /// <summary>
+        /// Determina si existe un término en el documento.
+        /// </summary>
+        /// <param name="term">Término consultado.</param>
+        /// <returns>True si el término existe dentro del documento. False en caso contrario.</returns>
         public bool hasTermino( string term ) {
             return Terminos.Exists(x => x.ToString() == term);
         }
 
+        /// <summary>
+        /// Carga el documento XML en la memoria.
+        /// </summary>
         public void cargarXmlDocument() {
             XmlDocument = new XmlDocument();
             XmlDocument.Load(Ruta);
         }
 
-        private string getTaxonDescription() {
-            XmlElement treatment = XmlDocument.DocumentElement;
-            XmlElement description = (XmlElement) treatment.GetElementsByTagName("description")[0];
-            return description.GetAttributeNode("taxon_description").Value;
+        /// <summary>
+        /// Obtiene el valor del atributo "taxon_description" del documento.
+        /// </summary>
+        /// <returns></returns>
+        public string getTaxonDescription() {
+            if (XmlDocument == null)
+            {
+                cargarXmlDocument();
+            }
+            try
+            {
+                XmlElement treatment = XmlDocument.DocumentElement;
+                XmlNode node = treatment.GetElementsByTagName("description")[0];
+                return node.Attributes["taxon_description"].Value;
+            }
+            catch (NullReferenceException e)
+            {
+                return null;
+            }
         }
 
-        private string getTaxonName() {
-            XmlElement treatment = XmlDocument.DocumentElement;
-            XmlElement description = (XmlElement) treatment.GetElementsByTagName("taxon_identification")[0];
-            return description.GetAttributeNode("taxon_name").Value;
+        /// <summary>
+        /// Obtiene el valor del atributo "taxon_name" del documento.
+        /// </summary>
+        /// <returns></returns>
+        public string getTaxonName() {
+            // Si el documento XML no está abierto, se carga.
+            if (XmlDocument == null)
+            {
+                cargarXmlDocument();
+            }
+            try
+            {
+                XmlElement treatment = XmlDocument.DocumentElement;
+                XmlNode node = treatment.GetElementsByTagName("taxon_identification")[0];
+                return node.Attributes["taxon_name"].Value;
+            }
+            catch (NullReferenceException e)
+            {
+                return null;
+            }
         }
 
-        private string getRank() {
+        /// <summary>
+        /// Obtiene el valor del atributo "rank" del documento.
+        /// </summary>
+        /// <returns></returns>
+        public string getRank() {
+            // Si el documento XML no está abierto, se carga.
+            if (XmlDocument == null)
+            {
+                cargarXmlDocument();
+            }
+
+            try
+            {
+                XmlElement treatment = XmlDocument.DocumentElement;
+                XmlNode node = treatment.GetElementsByTagName("taxon_identification")[0];
+                return node.Attributes["rank"].Value;
+            }
+            catch (NullReferenceException e)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene una lista con todos los valores del atributo "name" para todos los nodos "biological_entity" del documento.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> getBiologicalEntitiesNames()
+        {
+            // Si el documento XML no está abierto, se carga.
+            if (XmlDocument == null)
+            {
+                cargarXmlDocument();
+            }
+
             XmlElement treatment = XmlDocument.DocumentElement;
-            XmlElement description = (XmlElement) treatment.GetElementsByTagName("taxon_identification")[0];
-            return description.GetAttributeNode("rank").Value;
+            XmlNodeList nodes = treatment.GetElementsByTagName("biological_entity");
+            List<string> values = new List<string>();
+
+            foreach (XmlNode node in nodes)
+            {
+                try
+                {
+                    values.Add(node.Attributes["name"].Value);
+                }
+                catch (NullReferenceException)
+                {
+                    continue;
+                }
+            }
+            return values;
+        }
+
+        /// <summary>
+        /// Obtiene una lista con pares de valores para los atributos "name" y "value" de los nodos "character" asociados a un nodo "biological_entity" del documento.
+        /// </summary>
+        /// <param name="biologicalEntityName">Valor del atributo "name" para el "biological_entity" consultado.</param>
+        /// <returns>Lista con pares de valores para los atributos "name" y "value" para cada "character". 
+        /// Si el "biological_entity" no existe, se retorna Null.
+        /// Si algún "character" no tiene valores para algún atributo, dicho atributo se devuelve como un string vacío ("").
+        /// </returns>
+        public List<string[]> getBiologicalEntityCharacters(string biologicalEntityName)
+        {
+            // Si el documento XML no está abierto, se carga.
+            if (XmlDocument == null)
+            {
+                cargarXmlDocument();
+            }
+
+            List<string[]> charactersList = new List<string[]>();
+            XmlElement treatment = XmlDocument.DocumentElement;
+            try
+            {
+                XmlNodeList biologicalEntities = treatment.GetElementsByTagName("biological_entity");
+                foreach(XmlNode entity in biologicalEntities)
+                {
+                    if (entity.Attributes["name"].Value.Equals(biologicalEntityName))
+                    {
+                        // Verificar esta expresión XPath para obtener los "character" del "biological_entity".
+                        XmlNodeList characters = entity.SelectNodes(".//character");
+
+                        foreach (XmlNode character in characters)
+                        {
+                            string character_name = character.Attributes["name"].Value;
+                            string character_value = character.Attributes["value"].Value;
+                            if (character_name == null)
+                            {
+                                character_name = "";
+                            }
+                            if (character_value == null)
+                            {
+                                character_value = "";
+                            }
+                            charactersList.Add(new string[] { character_name, character_value });
+                        }
+                        return charactersList;
+                    }
+                }
+                return null;
+            }
+            catch (NullReferenceException e)
+            {
+                return null;
+            }
         }
 
         public List<string> getTerminos() {
